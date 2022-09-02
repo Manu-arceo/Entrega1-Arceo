b23c09d94fa9885524ece474b2e19a99bd6f4e84
Manuel Arceo <manu_arceo96@hotmail.com>
2022-09-02 12:29:19 -0300

    Version: Correciones

--- src/turismoBahia/forms.py
+++ src/turismoBahia/forms.py
@@ -30,10 +30,11 @@ class CreacionUsuarios(UserCreationForm):
     email = EmailField()
     password1 = CharField(label="Constraseña", widget=PasswordInput)
     password2 = CharField(label="Confirmar contraseña", widget=PasswordInput)
-
+    first_name = CharField(label="Nombre")
+    last_name = CharField(label="Apellido")
     class Meta: 
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ["first_name","last_name","email","username","password1", "password2"]
         help_texts = {"username": "", "email": "", "password1": "","password2": "" }
 
 
